bf35e35e1162b968392c3511b2827e1e503c28a6
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-08-07 23:24:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ if [ -d "/home/$USER/.oh-my-zsh" ]; then
 else
   echo "Installing oh my zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  chsh -s $(which zsh) $USER
+  sudo chsh -s $(which zsh) $USER
   REBOOT=true
 fi
 
