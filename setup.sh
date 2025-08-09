9f86a01d2295566a45c4756e325edfb1592d7a0b
Michael Gillett <51103663+migillett@users.noreply.github.com>
2025-08-09 17:36:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,85 +8,121 @@ REBOOT=false
 # exit if ANY steps in this process fails
 set -e
 
+### FUNCTIONS
+system_dependencies(){
+  echo "Running apt update and upgrade"
+  sudo apt update -qq && sudo apt upgrade -y -o Dpkg::Options::="--force-confnew" -qq
+
+  echo "Installing dependencies"
+  sudo apt install wget lunzip jq git zsh pipx rpi-connect-lite i2c-tools -y -qq
+
+  echo "Cleaning up unused dependencies"
+  sudo apt purge -y exim4-base exim4-config exim4-daemon-light -qq
+  sudo apt autoremove -y -qq
+  sudo apt clean -y -qq
+
+  echo "Installing Meshtastic CLI and Contact TUI"
+  pipx install meshtastic && pipx install contact && pipx ensurepath
+}
+
+
+check_spi() {
+  ### Enable SPI if not already enabled
+  echo "Checking if SPI is enabled..."
+  if command -v raspi-config &> /dev/null; then
+    SPI=$(raspi-config nonint get_spi)
+    if [ "$SPI" -eq 0 ]; then
+      echo "SPI is already enabled"
+    else
+      echo "SPI is not enabled, enabling now"
+      raspi-config nonint do_spi 0
+      REBOOT=true
+    fi
+  else
+    echo "raspi-config command not found. Please ensure you are running this on a Raspberry Pi OS."
+    exit 1
+  fi
+}
+
+
+oh_my_zsh_install() {
+  if [ -d "/home/$USER/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed"
+  else
+    echo "Installing oh my zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sudo chsh -s $(which zsh) $USER
+    REBOOT=true
+  fi
+}
+
+
+docker_setup() {
+  if ! command -v docker &> /dev/null; then
+    echo "Installing Docker..."
+    # Install Docker using the official installation script
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-docker.sh
+    echo "Docker installed successfully: $(docker --version)"
+  else
+    echo "Docker is already installed: $(docker --version)"
+  fi
+
+  # check if current user is in the docker group
+  if ! groups $USER | grep -q "\bdocker\b"; then
+    echo "Adding current user to the docker group"
+    sudo usermod -aG docker $USER
+    REBOOT=true
+  else
+    echo "Current user is already in the docker group"
+  fi
+}
+
+
+rpi_connect_setup() {
+  ### TURN ON RASPBERRY PI CONNECT
+  loginctl enable-linger
+  rpi-connect on
+  ### SIGNIN TO RPI CONNECT
+  rpi-connect signin
+}
+
+
+stop_unneeded_services() {
+  for service in bluetooth ModemManager
+  do
+    sudo systemctl stop "$service" # Quote $service for safety
+    sudo systemctl disable "$service" # Quote $service for safety
+    echo "Disabled $service service"
+  done
+}
+
 ### NON-INTERACTIVE CHECK
 if [ -z "${DEBIAN_FRONTEND}" ]; then
     export DEBIAN_FRONTEND=noninteractive
 fi
 
 ### SYSTEM UPDATE & DEPENDENCIES
-echo "Running apt update and upgrade"
-sudo apt update -qq && sudo apt upgrade -y -o Dpkg::Options::="--force-confnew" -qq
-
-echo "Installing dependencies"
-sudo apt install wget lunzip jq git zsh pipx rpi-connect-lite i2c-tools -y -qq
-
-echo "Cleaning up unused dependencies"
-sudo apt purge -y exim4-base exim4-config exim4-daemon-light -qq
-sudo apt autoremove -y -qq
-sudo apt clean -y -qq
-
-### RASPBERRY PI CONNECT
-loginctl enable-linger
-rpi-connect on
+system_dependencies()
 
 ### SPI
-if command -v raspi-config &> /dev/null; then
-  echo "Enabling SPI using raspi-config nonint"
-  sudo raspi-config nonint do_spi 0
-  REBOOT=true
-else
-  echo "raspi-config command not found, exiting"
-  exit 1
-fi
+check_spi()
 
 ### OH MY ZSH INSTALLATION
-if [ -d "/home/$USER/.oh-my-zsh" ]; then
-  echo "Oh My Zsh is already installed"
-else
-  echo "Installing oh my zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  sudo chsh -s $(which zsh) $USER
-  REBOOT=true
-fi
-
-### MESHTASTIC UTILITIES INSTALLATION
-echo "Installing Meshtastic CLI and Contact TUI"
-pipx install meshtastic && pipx install contact && pipx ensurepath
+oh_my_zsh_install()
 
 ### DOCKER INSTALLATION
-if ! command -v docker &> /dev/null; then
-  echo "Installing Docker..."
-  # Install Docker using the official installation script
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sudo sh get-docker.sh
-  rm get-docker.sh
-  echo "Docker installed successfully: $(docker --version)"
-else
-  echo "Docker is already installed: $(docker --version)"
-fi
+docker_setup()
 
-# check if current user is in the docker group
-if ! groups $USER | grep -q "\bdocker\b"; then
-  echo "Adding current user to the docker group"
-  sudo usermod -aG docker $USER
-  REBOOT=true
-else
-  echo "Current user is already in the docker group"
-fi
+### RPI CONNECT SETUP
+rpi_connect_setup()
 
 ### DISABLE UNNEEDED SERVICES
-for service in bluetooth ModemManager
-do
-  sudo systemctl stop "$service" # Quote $service for safety
-  sudo systemctl disable "$service" # Quote $service for safety
-  echo "Disabled $service service"
-done
+stop_unneeded_services()
 
 ### FINISHING UP
 echo "Setup complete!"
-
-### SIGNIN TO RPI CONNECT
-rpi-connect signin
 
 if [ "$REBOOT" = true ]; then
   echo "Rebooting system..."
